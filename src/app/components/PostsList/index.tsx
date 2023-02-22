d8e8d74155ac4dc938639b8c19feb653a63d7bc4
achu94 <aygyun_94@outlook.de>
2023-02-22 21:48:04 +0100

    Eingerückt.

--- src/app/components/PostsList/index.tsx
+++ src/app/components/PostsList/index.tsx
@@ -2,22 +2,22 @@ import React, { useEffect } from "react";
 import type { RootStore } from "../../store";
 import { useSelector, useDispatch } from "react-redux";
 import { setInitialData, removePost } from "../../features/PostsSlice";
-import { getData } from '../../utils/localStorage';
+import { getData } from "../../utils/localStorage";
 
 import PostElement from "../PostElement";
 import "./PostsList.css";
 
 const PostList = () => {
   const postsList = useSelector((state: RootStore) => state.posts);
-  const dispatch  = useDispatch();
+  const dispatch = useDispatch();
 
-  useEffect( () => {
-    (async ()=> {
+  useEffect(() => {
+    (async () => {
       const savedData = await getData("posts");
-      dispatch(setInitialData(savedData))
+      dispatch(setInitialData(savedData));
     })();
   }, []);
-  
+
   const removeHandler = (id: number) => dispatch(removePost(id));
 
   return (
@@ -26,7 +26,14 @@ const PostList = () => {
         <h1>Loading...</h1>
       ) : (
         postsList.map((data, index: number) => {
-          return <PostElement key={index} val={data.val} id={data.id} removeHandler={removeHandler}/>;
+          return (
+            <PostElement
+              key={index}
+              val={data.val}
+              id={data.id}
+              removeHandler={removeHandler}
+            />
+          );
         })
       )}
     </div>
